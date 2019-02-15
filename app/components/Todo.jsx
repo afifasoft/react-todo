39704baf5e4e0220f17c8fc166c456ee18e2d89d
afifasoft <abdul.pesit@gmail.com>
2019-02-15 07:58:27 +0400

    Add static todo list

---
+++ app/components/Todo.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+
+
+var Todo = React.createClass({
+  render: function () {
+    var {id, text} = this.props;
+    return (
+      <div>
+        {id}. {text}
+      </div>
+    );
+  }
+
+});
+
+module.exports = Todo;
