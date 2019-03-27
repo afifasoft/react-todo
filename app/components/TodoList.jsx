b3055de753345609fba18a7f903ceae3889388a9
afifasoft <abdul.pesit@gmail.com>
2019-03-27 22:36:01 +0400

    Add toggle support

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -7,7 +7,7 @@ var TodoList = React.createClass({
     var renderTodos = () => {
       return todos.map( (todo) => {
         return (
-          <Todo key={todo.id} {...todo}/> // spred operator
+          <Todo key={todo.id} {...todo} onToggle={this.props.onToggle}/> // spred operator
         );
       });
     };
